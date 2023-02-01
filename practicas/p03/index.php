abe01b54c1593cc3f7e1476b21d76a541a46d19e
VicZS <vicentezavsan@gmail.com>
2023-02-01 10:46:56 -0600

    Iniciso 2

--- practicas/p03/index.php
+++ practicas/p03/index.php
@@ -34,5 +34,47 @@
 
     ?>
 
+    <h2>Inciso 2</h2>
+    <p>2. Proporcionar los valores de $a, $b, $c como sigue: <br>
+    $a = “ManejadorSQL”; <br>
+    $b = 'MySQL’; <br>
+    $c = &$a;<br>
+    </p>
+
+   <?php
+   $a = "ManejadorSQL";
+   $b = 'MySQL';
+   $c = &$a;
+
+    echo 'a. Ahora muestra el contenido de cada variable <br> <br>';
+
+    echo 'Valor de $a: '.$a.'<br>';
+    echo 'Valor de $b: '.$b.'<br>';
+    echo 'Valor de $c: '.$c.'<br> <br>';
+
+
+    echo 'b. Agrega al código actual las siguientes asignaciones: <br>
+    $a = “PHP server”; <br>
+    $b = &$a; <br> <br>';
+
+    $a = "PHP server";
+    $b = &$a;
+
+
+    echo 'c. Vuelve a mostrar el contenido de cada uno <br> <br>';
+
+    echo 'Valor de $a: '.$a.'<br>';
+    echo 'Valor de $b: '.$b.'<br>';
+    echo 'Valor de $c: '.$c.'<br> <br>';
+
+    echo 'd. Describe en y muestra en la página obtenida qué ocurrió en el segundo bloque de <br>
+    asignaciones <br> <br>';
+
+    echo '$b y $c tienen el mismo valor que $a, ya que estos estan asignados por referencia a la variable $a';
+
+
+   ?>
+
+
 </body>
 </html>
